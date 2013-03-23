29b6219ae1969b4ad9c791d691ad97395f5e837d
David Kirkby <dkirkby@uci.edu>
2013-03-23 11:08:40 +0100

    Add check for heightfield file size

--- world/heightfield.py
+++ world/heightfield.py
@@ -1,4 +1,5 @@
 import sys
+import os.path
 import array
 
 """
@@ -11,16 +12,22 @@ class Heightfield:
     def __init__(self,filename,npixels):
         # create an empty array to hold the heightfield data as unsigned shorts
         self.data = array.array('H')
-        # open the file in binary read-only mode
-        with open(filename,"rb") as f:
-            try:
-                self.data.fromfile(f,npixels)
-            except EOFError:
-                print 'Not enough bytes in heightfield file',filename
+        try:
+            # check that the file has the expected size
+            if os.path.getsize(filename) != 2*npixels:
+                print 'Heightfield file has wrong size',filename
+            # open the file in binary read-only mode
+            with open(filename,"rb") as f:
+                try:
+                    self.data.fromfile(f,npixels)
+                except EOFError:
+                    print 'Unexpected EOF in heightfield file',filename
+        except (IOError,OSError):
+            print 'Unable to read heightfield file',filename
         # data in file is in network byte order (big endian) so swap here if necessary
         if sys.byteorder == "little":
             self.data.byteswap()
 
 if __name__ == "__main__":
-    h = Heightfield("world/data/poodle.hf",4800)
+    h = Heightfield("world/data/poodlex.hf",4800)
     print "ok"
