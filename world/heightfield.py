b01872550abf0c9763bbc38f1aa519422839d280
David Kirkby <dkirkby@uci.edu>
2013-03-23 11:18:28 +0100

    Heightfield ctor returns custom Error exception

--- world/heightfield.py
+++ world/heightfield.py
@@ -2,12 +2,19 @@ import sys
 import os.path
 import array
 
+class Error(Exception):
+    def __init__(self,msg):
+        self.msg = msg
+    def __str__(self):
+        return repr(self.msg)
+    
 """
 A heightfield represents the elevation relative to sea level on a pixelated spherical planet.
 """
 class Heightfield:
     """
     Reads a height field from a binary file using 16-bit signed integers for each pixel.
+    Raises a heightfield.Error in case of any problems.
     """
     def __init__(self,filename,npixels):
         # create an empty array to hold the heightfield data as unsigned shorts
@@ -15,19 +22,20 @@ class Heightfield:
         try:
             # check that the file has the expected size
             if os.path.getsize(filename) != 2*npixels:
-                print 'Heightfield file has wrong size',filename
+                raise Error('Heightfield file "%s" has wrong size %d (expected %d)' %
+                    (filename,os.path.getsize(filename),2*npixels))
             # open the file in binary read-only mode
             with open(filename,"rb") as f:
                 try:
                     self.data.fromfile(f,npixels)
                 except EOFError:
-                    print 'Unexpected EOF in heightfield file',filename
+                    raise Error('Unexpected EOF in heightfield file "%s"' % filename)
         except (IOError,OSError):
-            print 'Unable to read heightfield file',filename
+            raise Error('Unable to read heightfield file "%s"' % filename)
         # data in file is in network byte order (big endian) so swap here if necessary
         if sys.byteorder == "little":
             self.data.byteswap()
 
 if __name__ == "__main__":
-    h = Heightfield("world/data/poodlex.hf",4800)
+    h = Heightfield("world/data/poodle.hf",4800)
     print "ok"
