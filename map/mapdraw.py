c122ae4ce170fb9cfed44ae7bccbb3e6ed2299c4
Trevor Kirkby <trevor@raspberrypi.(none)>
2013-03-24 12:26:38 -0700

    mapdraw continued

--- map/mapdraw.py
+++ map/mapdraw.py
@@ -1,5 +1,6 @@
 import pygame, time
 pygame.init()
+thingDir = None
 
 def setMode(text):
 	if text == "w":
@@ -9,21 +10,61 @@ def setMode(text):
 		screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
 		return screen
 
-screen = setMode(raw_input("start in full screen or window? (f/w): "))
-try:
+def draw(screen):
 	surface = pygame.Surface((0,0))
 	surface.fill((0,65,0))
-	#screen.fill(surface)
+	surface.getrect
+	Surface.blit(surface, screen)
 	pygame.display.update()
-	time.sleep(10)
+	thingDirList = arrowInput()
+	count = 0
+	for boolean in thingDirList:
+		if boolean == True:
+			if count == 0:
+				thingDir == "right"
+			if count == 1:
+				thingDir == "left"
+			if count == 2:
+				thingDir == "down"
+			if count == 3:
+				thingDir == "up"
+		count = count + 1
+	print thingDir
+	return surface
+
+def arrowInput():
+	thingRight = False
+	thingLeft = False
+	thingDown = False
+	thingUp = False
+	for item in pygame.event.get():
+		if item.type == pygame.KEYDOWN and item.key == 275:
+			thingRight = True
+		if item.type == pygame.KEYUP and item.key == 275:
+			thingRight = False
+		if item.type == pygame.KEYDOWN and item.key == 273:
+			thingUp = True
+		if item.type == pygame.KEYUP and item.key == 273:
+			thingUp = False
+		if item.type == pygame.KEYDOWN and item.key == 274:
+			thingDown = True
+		if item.type == pygame.KEYUP and item.key == 274:
+			thingDown = False
+		if item.type == pygame.KEYDOWN and item.key == 276:
+			thingLeft = True
+		if item.type == pygame.KEYUP and item.key == 276: 
+			thingLeft = False
+		if item.type == pygame.KEYDOWN and item.key == 113:
+			raise SystemExit
+		return [thingRight,thingLeft,thingDown,thingUp]
+
+screen = setMode(raw_input("start in full screen or window? (f/w): "))
+try:
+	surface = draw(screen)
 except:
 	try:
 		screen = setMode(raw_input("please enter f for full screen or w for window: "))
-		surface = pygame.Surface((0,0))
-		surface.fill((0,65,0))
-		#screen.fill(surface)
-		pygame.display.update()
-		time.sleep(10)
+		surface = draw(screen)
 	except:
 		print "fine then! program shutting down..."
 		raise SystemExit
