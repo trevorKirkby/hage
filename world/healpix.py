05f58e6eca9c9c90801c31a8eb578d2d4461c050
David Kirkby <dkirkby@uci.edu>
2013-03-24 11:41:53 +0100

    Add pixel to coords calculation

--- world/healpix.py
+++ world/healpix.py
@@ -1,3 +1,5 @@
+import math
+
 class Error(Exception):
     def __init__(self,msg):
         self.msg = msg
@@ -18,5 +20,40 @@ class HEALPixMap:
             raise Error('Invalid nsides < 1 in HEALPixMap')
         self.nsides = nsides
         self.npixels = 12*nsides*nsides
+        self.ncap = 2*nsides*(nsides-1)
+    """
+    Tests if the specified pixel index is valid
+    """
     def isValidPixel(self,pixel):
         return pixel >= 0 and pixel < self.npixels
+    """
+    Returns the (i,j,z,phi) tuple for the specified pixel center
+    """
+    def getPixel(self,pixel):
+        if not self.isValidPixel(pixel):
+            raise Error('Invalid pixel index %d in getLatLon' % pixel)
+        if pixel < self.ncap:
+            # pixel is in north polar cap
+            i = int(math.floor(0.5*(1+math.floor(math.sqrt(2*pixel+1.5)))))
+            j = pixel+1-2*i*(i-1)
+            z = 1.0-i*i/(3.*self.nsides*self.nsides)
+            phi = math.pi/(2*i)*(j-0.5)
+        elif pixel < self.npixels - self.ncap:
+            # pixel is in the equatorial belt
+            ph = pixel-self.ncap
+            i = int(math.floor(ph/(4.0*self.nsides))+self.nsides)
+            j = ph%(4*self.nsides)+1
+            z = 2.0*(2*self.nsides-i)/(3.0*self.nsides)
+            if (i+self.nsides)%2 == 1:
+                s = 1.0
+            else:
+                s = 0.5
+            phi = math.pi/(2*self.nsides)*(j-s)
+        else:
+            # pixel is in the south polar cap
+            ph = self.npixels-pixel
+            i = int(math.floor(0.5*(1+math.floor(math.sqrt(2*ph-0.5)))))
+            j = 4*i+1-(ph-2*i*(i-1))
+            z = -1+i*i/(3.0*self.nsides*self.nsides)
+            phi = math.pi/(2*i)*(j-0.5)
+        return (i,j,z,phi)
--- worldview.py
+++ worldview.py
@@ -2,4 +2,6 @@ import world.heightfield
 
 hf = world.heightfield.Heightfield('world/data/poodle.40.hf',40)
 print hf.getHeight(100)
-
+print hf.pixmap.getPixel(100)
+print hf.pixmap.getPixel(4000)
+print hf.pixmap.getPixel(19000)
